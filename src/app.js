2a6d7bb1e9d4c754ccc22435c6b3044fdf321cd2
Yihan <sherry1570304111@gmail.com>
2025-07-10 22:10:56 -0700

    fix deployment

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); 
 
 const app = express();
 
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(express.json());
 
+app.get('/', (req, res, next) => res.send('👋 Express is live'));
+app.use(express.static(path.join(__dirname, '../public')));
 app.get('/api/hello', (req, res) => {
   res.json({ msg: 'Hello from backend!' });
 });
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}： http://localhost:${PORT}`);
 });
